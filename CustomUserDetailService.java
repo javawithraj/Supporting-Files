619a7ef3ffc085453687d3960e835f9277470ade
k rajasekhar <krajasekhar5085@gmail.com>
2025-10-06 17:55:12 +0530

    supporting files

---
+++ CustomUserDetailService.java
@@ -0,0 +1,21 @@
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		return repository.findByEmail(email).orElseThrow(()->new RuntimeException("Invalid Username"));
+	}
+	
+}
